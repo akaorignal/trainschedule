2d2c7954b0a0fa6f72d636c8732ae7d16b7672c7
akaorignal <torir.adams@gmail.com>
2017-09-05 22:06:27 -0700

    Update js2.js

--- js2.js
+++ js2.js
@@ -2,7 +2,7 @@
 // Steps to complete:
 // 1. Initialize Firebase
 // 2. Create button for adding new trains - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
+// 3. Create a way to retrieve trn from the employee database.
 // 4. Create a way to calculate the months worked. Using difference between start and current time.
 //    Then use moment.js formatting to set difference in months.
 // 5. Calculate Total billed
@@ -22,7 +22,7 @@
 
 
 
-// 2. Button for adding Employees
+// 2. Button for adding trn
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
   // Grabs user input
@@ -37,7 +37,7 @@ $("#add-train-btn").on("click", function(event) {
     start: empStart,
     freq: freq
   };
-  // Uploads employee data to the database
+  // Uploads trn data to the database
   database.ref().push(newTrn);
   // Logs everything to console
   console.log(newTrn.name);
